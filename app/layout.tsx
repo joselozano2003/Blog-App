292ae19f94642e32a557fd65bded9c25699d5709
JoseLozano2003 <josecamilo.lozanocet@ucalgary.ca>
2023-05-25 17:17:19 -0600

    User Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import AuthProvider from './AuthProvider'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import NavMenu from './NavMenu'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -13,9 +13,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+	return (
+    	<AuthProvider>
+      		<html lang="en">
+				<body>
+					<NavMenu />
+					{children}
+				</body>
+			</html>
+		</AuthProvider>
   )
 }
